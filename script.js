2dae76cc730aeb2d8b8e32873c75b802f5fc5f17
Davialcantarati <davialcantara.ti@gmail.com>
2020-07-19 17:02:16 -0300

    Including numeric classes

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function createPokemonCard(pokemon) {
        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
     </div>
     <div class="info">
-         <span class="number">#${pokemon.id}</span>
+         <span class="number">#${pokemon.id.toString().padStart(3,'0')}</span>
          <h3 class="name">${name}</h3>
          <small class="type">Type: <span>${type}</span></small>
     </div>
